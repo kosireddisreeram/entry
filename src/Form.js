f1e8ceb351dde09af4d8b1170528b13a62e2e087
sreeramquickworkz <sreeram.raj@quickworkz.co.in>
2021-10-08 12:31:14 +0530

    structure fo login form with wo divs align in center

---
+++ src/Form.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Form.css';
+const Form = () => {
+return (
+    <>
+      <div className='form-container'>
+        <div className='form-content-left'>
+          <img className='form-img' src='img/img-2.svg' alt='spaceship' />
+        </div>
+       </div>
+    </>
+  );
+};
+export default Form;
