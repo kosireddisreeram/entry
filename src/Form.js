519e4d3ebdf7b37f562e15dd9826be6a5656b435
sreeramquickworkz <sreeram.raj@quickworkz.co.in>
2021-10-09 11:01:04 +0530

    signup

--- src/Form.js
+++ src/Form.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+import img1 from './images/sign.png';
+
+
 const Form = () => {
-return (
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
     <>
       <div className='form-container'>
         <div className='form-content-left'>
-          <img className='form-img' src='img/img-2.svg' alt='spaceship' />
+          <img className='form-img' src={img1} alt='spaceship' />
         </div>
-       </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
     </>
   );
 };
+
 export default Form;
